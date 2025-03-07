0bb5c8757db7bf5c2884bbe598f940207692a936
Chase <summerhaysachase@gmail.com>
2025-03-07 18:21:50 -0500

    Updated EDA.ipynb and app.py with better project flow to include Titles, comments, and conclusions. Better data cleaning code, included groupby and filtered outliers for better quality histogram and scatterplots

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
-import streamlit as st
+# Load dataset
 import pandas as pd
 import plotly.express as px
+import streamlit as st
 
-# Load dataset
-df = pd.read_csv("vehicles_us.csv")
+df = pd.read_csv("C:\\Users\\Summe\\Caradvertisement\\vehicles_us.csv")
 
-# Fill missing values & convert data types
-df['paint_color'].fillna("unknown", inplace=True)
-df['is_4wd'].fillna(0, inplace=True)
-df['is_4wd'] = df['is_4wd'].astype(int)
-df['model_year'].fillna(df['model_year'].median(), inplace=True)
-df['cylinders'].fillna(df['cylinders'].mode()[0], inplace=True)
-df['model_year'] = df['model_year'].astype(int)
-df['cylinders'] = df['cylinders'].astype(int)
+
+# Replace NaN values in specified columns
+df = df.assign(
+    model_year=df['model_year'].fillna('unknown'),
+    cylinders=df['cylinders'].fillna('unknown'),
+    odometer=df['odometer'].fillna(0),
+    paint_color=df['paint_color'].fillna('unknown'),
+    is_4wd=df['is_4wd'].fillna(0)
+)
+
+
+# Convert model_year and cylinders columns to integer type
+df['model_year'] = df['model_year'].apply(pd.to_numeric, errors='coerce').fillna(0).astype(int)
+df['cylinders'] = df['cylinders'].apply(pd.to_numeric, errors='coerce').fillna(0).astype(int)
+
+# Remove outliers from 'model_year' column
+df = df[df['model_year'] > 1900]
+df = df[df['model_year'] < 2023]
+
+# Remove outliers from 'price' column
+df = df[df['price'] > 1000]
+df = df[df['price'] < 100000]
+
+# Remove outliers from 'odometer' column
+df = df[df['odometer'] > 1000]
+df = df[df['odometer'] < 400000]
+
+# Group by 'model' and calculate the median 'model_year' for each group
+median_years = df.groupby('model')['model_year'].median()
+
+# Group by 'model' and calculate the median 'cylinders' for each group
+median_cylinders = df.groupby('model')['cylinders'].median()
+
+# Group by 'model_year' and calculate the median 'odometer' for each group
+median_odometer = df.groupby('model_year')['odometer'].median()
 
 # Streamlit Header
-st.header("Car Advertisement Data Analysis 🚗")
+st.header("Car Advertisement Data Analysis")
 
 # Checkbox to filter price data
 filter_expensive = st.checkbox("Show only cars priced under $50,000")
